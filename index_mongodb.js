8f27a88c8f55ae2ac9efd065bdf9fca15d019b13
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-20 12:33:43 -0700

    added route handling for PUT helpful and reported

--- index_mongodb.js
+++ index_mongodb.js
@@ -14,6 +14,8 @@ app.use(cors());
 app.get('/reviews/', controller.getReviews);
 app.get('/reviews/meta', controller.getMeta);
 app.post('/reviews', controller.addReview);
+app.put('/reviews/:review_id/helpful', controller.markHelpful);
+app.put('/reviews/:review_id/report', controller.markReported);
 
 const PORT = 1337;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
