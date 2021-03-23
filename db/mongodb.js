9571fbdecc85f5b70b646e5e8655551af2d2540f
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-22 23:16:42 -0700

    added url to db server

--- db/mongodb.js
+++ db/mongodb.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
 
+// mongoose.connect('mongodb://54.183.228.228/review-service', { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('mongodb://localhost/review-service', { useNewUrlParser: true, useUnifiedTopology: true });
 module.exports = mongoose;
