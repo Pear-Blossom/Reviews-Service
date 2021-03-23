6c0ab9c23c0f605b8f44500af519da26c95f74f8
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-23 09:46:32 -0700

    added loaderio verification

--- index_postgres.js
+++ index_postgres.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
@@ -15,6 +16,8 @@ app.get('/reviews/meta', controller.getMeta);
 app.post('/reviews', controller.addReview);
 app.put('/reviews/:review_id/helpful', controller.markHelpful);
 app.put('/reviews/:review_id/report', controller.markReported);
+
+app.get('/loaderio-3faed01e946339b6a786df6617f44c45', (req, res) => res.sendFile(path.resolve('loaderio.txt')));
 
 const PORT = 1337;
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
